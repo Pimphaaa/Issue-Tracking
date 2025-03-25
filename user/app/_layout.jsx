8f18d815c8f96a5e27d749a89b445684fdc9e6f8
paweethida-1 <paweethidakham@gmail.com>
2025-03-25 21:35:57 +0700

    commit

--- user/app/_layout.jsx
+++ user/app/_layout.jsx
@@ -2,10 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import TabBar from '../components/TabBar'
+
+
+
+
 const _layout = () => {
   return (
     <Tabs
-        tabBar={proms=> <TabBar {...proms} />}
+        tabBar={props=> <TabBar {...props} />}
     >
         <Tabs.Screen
             name="Home"
--- user/components/TabBar.jsx
+++ user/components/TabBar.jsx
@@ -2,44 +2,42 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 
+const TabBar = ({ state, descriptors, navigation }) => {
+return (
+  <View style={styles.tabbar}>
+    {state.routes.map((route, index) => {
+      const { options } = descriptors[route.key];
+      const label =
+        options.tabBarLabel !== undefined
+          ? options.tabBarLabel
+          : options.title !== undefined
+          ? options.title
+          : route.name;
+
+      if(['_sitemap', '+not-found'].includes(route.name)) return null;
 
 
-const TabBar = ({state, descriptors, navigation}) => {
-  return (
-    <View style={{ flexDirection: 'row' }}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-              ? options.title
-              : route.name;
+      const isFocused = state.index === index;
 
+      const onPress = () => {
+        const event = navigation.emit({
+          type: 'tabPress',
+          target: route.key,
+          canPreventDefault: true,
+        });
 
-            
+        if (!isFocused && !event.defaultPrevented) {
+          navigation.navigate(route.name, route.params);
+        }
+      };
 
+      const onLongPress = () => {
+        navigation.emit({
+          type: 'tabLongPress',
+          target: route.key,
+        });
+      };
 
-        const isFocused = state.index === index;
-
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params);
-          }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
 
         return (
           <TouchableOpacity
